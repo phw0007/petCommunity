2c71a1115c1587ae71f51c94fdeb7a0fb5674544
hhh7441 <140154341+hhh7441@users.noreply.github.com>
2023-08-07 14:41:50 +0900

    Delete main directory

--- main/java/com/care/project/alogin/LoginMapper.java
+++
@@ -1,8 +0,0 @@
-package com.care.project.alogin;
-
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface LoginMapper {
-
-}
--- main/java/com/care/project/site/SiteMapper.java
+++
@@ -1,8 +0,0 @@
-package com.care.project.site;
-
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface SiteMapper {
-
-}
